5c8bf8a02f33c212a5646849ea91297ff6191b39
ikka0u0a <131534303+ikka0u0a@users.noreply.github.com>
2023-07-01 11:12:16 +0900

    Add answer for APG4b_e

---
+++ src/APG4b/e.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  // 一年の秒数
+  int seconds = 365 * 24 * 60 * 60;
+ 
+  cout << seconds << endl; //1年は何秒か
+  cout << seconds * 2 << endl; //2年は何秒か
+  cout << seconds * 5 << endl; //5年は何秒か 
+  cout << seconds * 10 << endl; //10年は何秒か
+}
