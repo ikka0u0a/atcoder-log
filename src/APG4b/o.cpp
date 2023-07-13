2281c2c7d538cf670560b4d121fe8531ade51d17
ikka0u0a <131534303+ikka0u0a@users.noreply.github.com>
2023-07-13 18:48:19 +0900

    Update answer for APG4b_o

--- src/APG4b/o.cpp
+++ src/APG4b/o.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main() {
   int A, B, C;
   cin >> A >> B >> C;
-  int m = min(A, B);
-  m = min(m, C);
-  int mx = max(A, B);
-  mx = max(mx, C);
-  cout << mx - m << endl;
+ 
+  int maximum = max(max(A, B), C); // 最大値を計算
+  int minimum = min(min(A, B), C); // 最小値を計算
+ 
+  cout << maximum - minimum << endl;
 }
