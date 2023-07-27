f9139b8649d6fc18d8696f5ee96db4d013a9451b
ikka0u0a <131534303+ikka0u0a@users.noreply.github.com>
2023-07-27 15:34:50 +0900

    Add answer for abc212_c

--- src/abc/212/c.cpp
+++ src/abc/212/c.cpp
@@ -8,27 +8,19 @@ int main() {
   vector<int> A(N);
   vector<int> B(M);
 
-  rep(i,N){
-    cin >> A.at(i);
-  }
-  rep(i,M){
-    cin >> B.at(i);
-  }
-  int ans = -1;
-  rep(i,N){
-    rep(j,M){
-      int r = 0;
-      if(A.at(i) >= B.at(j)){
-        r = A.at(i) - B.at(j);
-      } else {
-        r = B.at(j) - A.at(i);
-      }
-      if(ans == -1){
-         ans = r;
-      }else if(ans > r){
-        ans = r;
-      }
-    }
-  }
+  rep(i,N) cin >> A.at(i);
+  rep(i,M) cin >> B.at(i);
+
+  sort(A.begin(), A.end());
+  sort(B.begin(), B.end());
+
+  int x = 0;
+  int y = 0;
+  int ans = abs(A.at(0) - B.at(0)); //abs(x)引数xの絶対値を返す
+  while ((x < N) && (y < M)) {
+		ans = min(ans, abs(A.at(x) - B.at(y)));
+		if (A.at(x) > B.at(y)) y++;
+		else x++;
+	}
   cout << ans << endl;
 }
