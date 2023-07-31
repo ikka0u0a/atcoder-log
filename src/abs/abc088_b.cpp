2692db98664c95bd269ffdcff648eb49af300421
ikka0u0a <131534303+ikka0u0a@users.noreply.github.com>
2023-07-31 10:09:44 +0900

    Add answer for abs_abc088_b

---
+++ src/abs/abc088_b.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  int N;
+  cin >> N;
+  vector<int> a(N);
+  rep(i,N){
+    cin >> a.at(i);
+  }
+  sort(a.begin(),a.end());
+  reverse(a.begin(),a.end());
+  
+  int ans = 0;
+  rep(i,N){
+    if(i % 2 == 0) ans += a.at(i);
+    else ans -= a.at(i);
+  }
+
+  cout << ans << endl;
+}
