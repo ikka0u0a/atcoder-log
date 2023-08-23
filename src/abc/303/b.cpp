630bec1d238994e325a5f983868899d52a763a70
ikka0u0a <131534303+ikka0u0a@users.noreply.github.com>
2023-08-23 17:44:21 +0900

    Add answer for abc303_b(RE)

---
+++ src/abc/303/b.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  int N,M;
+  cin >> N >> M;
+  vector<vector<string>> A(N, vector<string>(M));
+  rep(i, M) rep(j, N) cin >> A[i][j];
+  set <string> S;
+  rep(i, M) rep(j, N - 1) {
+    S.insert(A[i][j] + A[i][j + 1]);
+    S.insert(A[i][j + 1] + A[i][j]);
+  }
+  int ans = 0;
+  for(int i = 1; i <= N; i++){
+    for(int j = 1; j <= N; j++){
+      if(i == j) continue;
+      else {
+        string t = to_string(i) + to_string(j);
+        if(!S.count(t)) ans++;
+      }
+    }
+  }
+  if(ans != 0) ans /= 2;
+  cout << ans << endl;
+}
