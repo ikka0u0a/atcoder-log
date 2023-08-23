2cf37aad49810dc296045e55301ce861bfb43130
ikka0u0a <131534303+ikka0u0a@users.noreply.github.com>
2023-08-23 20:18:49 +0900

    Add answer for abc303_b

--- src/abc/303/b.cpp
+++ src/abc/303/b.cpp
@@ -9,15 +9,15 @@ int main() {
   rep(i, M) rep(j, N) cin >> A[i][j];
   set <string> S;
   rep(i, M) rep(j, N - 1) {
-    S.insert(A[i][j] + A[i][j + 1]);
-    S.insert(A[i][j + 1] + A[i][j]);
+    S.insert(A[i][j] + ":" + A[i][j + 1]);
+    S.insert(A[i][j + 1] + ":" + A[i][j]);
   }
   int ans = 0;
   for(int i = 1; i <= N; i++){
     for(int j = 1; j <= N; j++){
       if(i == j) continue;
       else {
-        string t = to_string(i) + to_string(j);
+        string t = to_string(i) + ":" + to_string(j);
         if(!S.count(t)) ans++;
       }
     }
