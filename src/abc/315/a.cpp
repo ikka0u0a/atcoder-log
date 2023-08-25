507ba4f8efada13c08b3825a1262bb6c7724116b
ikka0u0a <131534303+ikka0u0a@users.noreply.github.com>
2023-08-25 18:09:25 +0900

    Add answer for abc315_a

---
+++ src/abc/315/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  string S;
+  cin >> S;
+  rep(i,S.size()){
+    if(S[i] != 'a' && S[i] != 'e' && S[i] != 'i' && S[i] != 'o' && S[i] != 'u'){
+      cout << S[i];
+    }
+  }
+  cout << endl;
+}
