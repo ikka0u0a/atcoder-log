ad5bade54b842950218cf553740d350c908e21e8
ikka0u0a <131534303+ikka0u0a@users.noreply.github.com>
2023-09-27 12:41:48 +0900

    Add answer for abc320_b(upsolve)

---
+++ src/abc/320/b.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+bool isPalindrom(string s){
+  string t = s;
+  reverse(t.begin(),t.end());
+  return s == t;
+}
+
+int main() {
+  string s;
+  cin >> s;
+  int n = s.size();
+  int ans = 0;
+  rep(r,n)rep(l,r+1){
+    string ns = s.substr(l,r-l+1);
+    if(isPalindrom(ns)) ans = max(ans, r-l+1);
+  }
+  cout << ans << endl;
+  return 0;
+}
